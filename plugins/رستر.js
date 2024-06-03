b8eea89b8e044c70a28ea451527bac9b44da306b
safrotbot <safrotbob3@gmail.com>
2024-06-03 20:05:13 +0300

    Delete plugins directory

--- plugins/رستر.js
+++
@@ -1,8 +0,0 @@
-let handler = async(m, { conn }) => {
-let revoke = await conn.groupRevokeInvite(m.chat)
-await conn.reply(m.chat, `تم تغير رابط الجروب بنجاح 💫\n✨🫡 الرابط الجديد: ${'https://chat.whatsapp.com/' + revoke}`, m)}
-handler.command = ['revoke', 'رستر', 'ريستارت', 'إعادة'] 
-handler.botAdmin = true['✓ ◡̈⃝ ✓│رستر🧸💜']
-handler.admin = ['group']
-handler.group = true
-export default handler
